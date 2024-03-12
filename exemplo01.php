5b4355b782b6192309044e6117a36ee2413c007e
jumalisilva <juliamalisilva@gmail.com>
2024-03-12 15:57:30 -0300

    php - aula 01

---
+++ exemplo01.php
@@ -0,0 +1,12 @@
+<?php
+
+    $num1 = 10;
+    $num2 = '10';
+    $nome = "Senac Largo Treze";
+    echo 'O valor de num1 é ' . $num1 . '<br />'; // o . é para concatenar
+    echo 'O valor de num2 é ' . $num2;
+    echo "<br />O valor de $num1 é  valor de $num2 e nome $nome";
+    // echo '<br />O valor de $num1 é  valor de $num2 e nome $nome'; - com aspas simples, tudo vira string
+    // o echo não dá retorno se deu sucesso ou não, enquanto o print sim
+
+?>
